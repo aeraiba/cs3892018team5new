088f259d82602af7ec08d032724ca1ed4d9ef55f
aeraiba <amireraiba@yahoo.com>
2018-05-01 16:01:02 -0400

    Add files via upload

--- app/src/main/java/pace/cs3892018team5/dev/chinesefortuneaide/Score.java
+++ app/src/main/java/pace/cs3892018team5/dev/chinesefortuneaide/Score.java
@@ -15,15 +15,12 @@ public class Score {
     private transient Context ctx;
     private transient Gson gson = new Gson();
 
-    public static Score get(Context ctx) {
+    public static Score get(Context ctx){
         String scJson = ctx.getSharedPreferences(ctx.getPackageName(), Context.MODE_PRIVATE).getString(SCORE_PREF_NAME, null);
-         if (scJson == null)
+        if(scJson==null)
             return new Score(ctx);
-
-          else{
-
-
-               Score s = new Gson().fromJson(scJson, Score.class);
+        else{
+            Score s = new Gson().fromJson(scJson, Score.class);
             s.ctx = ctx;
             s.gson = new Gson();
             return s;
@@ -31,30 +28,26 @@ public class Score {
     }
 
     private Score(Context ctx){
-
         this.ctx = ctx;
     }
 
     public void add(int qid, boolean stat){
-
         qaStat.put(qid, stat);
     }
 
     public int getRating(){
         Integer[] keys = qaStat.keySet().toArray(new Integer[]{});
-
-        if(keys.length==0)
-            return 0;
-          int total = keys.length;
-         int correct = 0;
+        if(keys.length==0) return 0;
+        int total = keys.length;
+        int correct = 0;
         for(Integer key:keys)
             if(qaStat.get(key)==true)
                 correct++;
-        return (5/total)*correct;
+        return (4/total)*correct;
     }
 
     public void save(){
-          SharedPreferences.Editor editor = ctx.getSharedPreferences(ctx.getPackageName(), Context.MODE_PRIVATE).edit();
+        SharedPreferences.Editor editor = ctx.getSharedPreferences(ctx.getPackageName(), Context.MODE_PRIVATE).edit();
         editor.putString(SCORE_PREF_NAME, gson.toJson(this));
         editor.commit();
     }
